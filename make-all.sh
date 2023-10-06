c53fac1e461e44f73113a3c88ed79404609f0ee5
Anthony Cole <anthony.cole@codasip.com>
2023-10-06 20:29:34 +0100

    Update to latest interrupt map and remove User GPIO Interrupts and demo (hello_world_interrupt)

--- make-all.sh
+++ make-all.sh
@@ -5,7 +5,7 @@
 ./make-sample.sh codasip_inferno_l31carbon     samples                  synchronization
 ./make-sample.sh codasip_inferno_l31carbon     samples                  philosophers
 ./make-sample.sh codasip_inferno_l31carbon     samples/drivers/uart     echo_bot               echo_bot-rx_interrupt
-./make-sample.sh codasip_inferno_l31carbon     samples/codasip          hello_world_interrupt
+#./make-sample.sh codasip_inferno_l31carbon     samples/codasip          hello_world_interrupt USER TEST INTERRUPTS HAVE BEEN REMOVED FROM THE RTL
 ./make-sample.sh codasip_inferno_l31carbon     samples/codasip          hello_world_user
 ./make-sample.sh codasip_inferno_l31carbon     samples/codasip          fat_fs
 
@@ -14,7 +14,7 @@
 ./make-sample.sh codasip_inferno_l31helium     samples                  synchronization
 ./make-sample.sh codasip_inferno_l31helium     samples                  philosophers
 ./make-sample.sh codasip_inferno_l31helium     samples/drivers/uart     echo_bot               echo_bot-rx_interrupt
-./make-sample.sh codasip_inferno_l31helium     samples/codasip          hello_world_interrupt
+#./make-sample.sh codasip_inferno_l31helium     samples/codasip          hello_world_interrupt
 ./make-sample.sh codasip_inferno_l31helium     samples/codasip          hello_world_user
 ./make-sample.sh codasip_inferno_l31helium     samples/codasip          fat_fs
 
@@ -30,24 +30,24 @@
 ./make-sample.sh codasip_iiot_rp_l31fluorine   samples                  synchronization       # Task switch & synchronisation demo
 ./make-sample.sh codasip_iiot_rp_l31fluorine   samples                  philosophers          # Task switch & synchronisation demo
 ./make-sample.sh codasip_iiot_rp_l31fluorine   samples/drivers/uart     echo_bot               echo_bot-rx_interrupt # UART and interrupt demo
-./make-sample.sh codasip_iiot_rp_l31fluorine   samples/codasip          hello_world_interrupt # UART and interrupt demo
+#./make-sample.sh codasip_iiot_rp_l31fluorine   samples/codasip          hello_world_interrupt # UART and interrupt demo
 ./make-sample.sh codasip_iiot_rp_l31fluorine   samples/codasip          hello_world_user      # User mode PMP access violation demo
 ./make-sample.sh codasip_iiot_rp_l31fluorine   samples/codasip          fat_fs                # SD card access demo
 
-./make-sample.sh codasip_ghostship_l31carbon   samples/codasip          blinkers
-./make-sample.sh codasip_ghostship_l31carbon   samples/subsys/console   getchar
-./make-sample.sh codasip_ghostship_l31carbon   samples                  synchronization
-./make-sample.sh codasip_ghostship_l31carbon   samples                  philosophers
-./make-sample.sh codasip_ghostship_l31carbon   samples/drivers/uart     echo_bot               echo_bot-rx_interrupt
-./make-sample.sh codasip_ghostship_l31carbon   samples/codasip          hello_world_interrupt
-./make-sample.sh codasip_ghostship_l31carbon   samples/codasip          hello_world_user
-./make-sample.sh codasip_ghostship_l31carbon   samples/codasip          fat_fs
+# ./make-sample.sh codasip_ghostship_l31carbon   samples/codasip          blinkers
+# ./make-sample.sh codasip_ghostship_l31carbon   samples/subsys/console   getchar
+# ./make-sample.sh codasip_ghostship_l31carbon   samples                  synchronization
+# ./make-sample.sh codasip_ghostship_l31carbon   samples                  philosophers
+# ./make-sample.sh codasip_ghostship_l31carbon   samples/drivers/uart     echo_bot               echo_bot-rx_interrupt
+# ./make-sample.sh codasip_ghostship_l31carbon   samples/codasip          hello_world_interrupt
+# ./make-sample.sh codasip_ghostship_l31carbon   samples/codasip          hello_world_user
+# ./make-sample.sh codasip_ghostship_l31carbon   samples/codasip          fat_fs
 
-./make-sample.sh codasip_ghostship_l31helium   samples/codasip          blinkers
-./make-sample.sh codasip_ghostship_l31helium   samples/subsys/console   getchar
-./make-sample.sh codasip_ghostship_l31helium   samples                  synchronization
-./make-sample.sh codasip_ghostship_l31helium   samples                  philosophers
-./make-sample.sh codasip_ghostship_l31helium   samples/drivers/uart     echo_bot               echo_bot-rx_interrupt
-./make-sample.sh codasip_ghostship_l31helium   samples/codasip          hello_world_interrupt
-./make-sample.sh codasip_ghostship_l31helium   samples/codasip          hello_world_user
-./make-sample.sh codasip_ghostship_l31helium   samples/codasip          fat_fs
+# ./make-sample.sh codasip_ghostship_l31helium   samples/codasip          blinkers
+# ./make-sample.sh codasip_ghostship_l31helium   samples/subsys/console   getchar
+# ./make-sample.sh codasip_ghostship_l31helium   samples                  synchronization
+# ./make-sample.sh codasip_ghostship_l31helium   samples                  philosophers
+# ./make-sample.sh codasip_ghostship_l31helium   samples/drivers/uart     echo_bot               echo_bot-rx_interrupt
+# ./make-sample.sh codasip_ghostship_l31helium   samples/codasip          hello_world_interrupt
+# ./make-sample.sh codasip_ghostship_l31helium   samples/codasip          hello_world_user
+# ./make-sample.sh codasip_ghostship_l31helium   samples/codasip          fat_fs
